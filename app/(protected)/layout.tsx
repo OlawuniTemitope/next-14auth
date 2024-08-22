49c74e52f1c9deefa79888e7850001fcbe1d63e5
Temitope Olawuni <olawunitemitope68@gmail.com>
2024-08-22 13:17:11 +0100

    screen size

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -8,7 +8,7 @@ interface ProtectedLayoutProps {
 const ProtectedLayout = ({children}:ProtectedLayoutProps) => {
   return (
     <div className=" flex h-full items-center justify-center gap-y-10
-    w-full flex-col bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
+    w-screen flex-col bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
     from-sky-400 to-blue-800">
         <Navbar/>
         {children}
