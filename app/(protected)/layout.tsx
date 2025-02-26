a287743901762b599f7f5b15ae1b9caabba5fed7
Topman <111436993+OlawuniTemitope@users.noreply.github.com>
2025-02-26 09:19:26 +0100

    Update layout.tsx
    screen size

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -7,7 +7,7 @@ interface ProtectedLayoutProps {
 
 const ProtectedLayout = ({children}:ProtectedLayoutProps) => {
   return (
-    <div className=" flex h-full items-center justify-center gap-y-10
+    <div className=" flex h-[90%] items-center justify-center gap-y-10
     w-screen flex-col bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
     from-sky-400 to-blue-800">
         <Navbar/>
